c0531ec4e4b5435be3780d2054c733f03a1eb7e7
PlanSK <skapkanshchikov@gmail.com>
2024-08-14 22:11:57 +0300

    Move code constants to settings.py

--- main.py
+++ main.py
@@ -2,25 +2,28 @@ import keyboard
 from time import sleep
 from loguru import logger
 
-ITERATION_COUNTER = 1
-COMMAND = "enter"
+from .settings import *
 
 
 def command_execute():
-    keyboard.write("ls")
-    keyboard.press(COMMAND)
-    sleep(10)
-    logger.debug("commands executed.")
+    keyboard.send(COMMAND)
+    sleep(BETWEEN_ITERATIONS_DELAY)
+    logger.debug("Iteration completed")
 
 
 if __name__ == "__main__":
     keyboard.wait("space")
-    logger.debug("Space button pushed.")
+    logger.debug(
+        f"Space button pushed. Switch the window. And waiting {SWITCH_TIMESTAMP} seconds."
+    )
     keyboard.send("alt+tab")
-    sleep(2)
+    sleep(SWITCH_TIMESTAMP)
+    logger.debug("Run commands.")
     if ITERATION_COUNTER > 0:
+        logger.debug(f"Limited iterations ({ITERATION_COUNTER}) start.")
         for _ in range(ITERATION_COUNTER):
             command_execute()
     else:
+        logger.debug("Unlimited iterations start.")
         while True:
             command_execute()
