40094546b04aedacd6d0059ce8ade716a9712fb0
PlanSK <skapkanshchikov@gmail.com>
2024-08-14 16:06:40 +0300

    Add logging and iteration counter.

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import keyboard
 from time import sleep
+from loguru import logger
+
+ITERATION_COUNTER = 1
+COMMAND = "enter"
+
+
+def command_execute():
+    keyboard.write("ls")
+    keyboard.press(COMMAND)
+    sleep(10)
+    logger.debug("commands executed.")
 
 
 if __name__ == "__main__":
-    keyboard.wait('space')
-    keyboard.send('alt+tab')
+    keyboard.wait("space")
+    logger.debug("Space button pushed.")
+    keyboard.send("alt+tab")
     sleep(2)
-    keyboard.write('ls')
-    keyboard.press('enter')
-    print('Space key pushed.')
+    if ITERATION_COUNTER > 0:
+        for _ in range(ITERATION_COUNTER):
+            command_execute()
+    else:
+        while True:
+            command_execute()
